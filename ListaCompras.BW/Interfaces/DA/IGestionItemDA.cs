e44ae55da737c4d19375677eae6dbf173b8d0cf6
L1mItrIx <kazeltro@gmail.com>
2025-10-24 14:29:40 -0600

    added interfaces, still incomplete

---
+++ ListaCompras.BW/Interfaces/DA/IGestionItemDA.cs
@@ -0,0 +1,14 @@
+﻿using ListaCompras.BC.Modelos;
+
+namespace ListaCompras.BW.Interfaces.DA
+{
+    public class IGestionItemDA
+    {
+        Task<bool> AgregarProducto(ItemLista item);
+
+        Task<List<ItemLista>> ObtenerProductoPendiente();
+        Task<bool> EliminarProducto(Guid idItem);
+
+        Task<List<ItemLista>> MarcarProducto(ItemLista item, Enum ItemEstado);
+    }
+}
---
+++ ListaCompras.BW/Interfaces/DA/IGestionListaDA.cs
@@ -0,0 +1,15 @@
+﻿using ListaCompras.BC.Modelos;
+
+namespace ListaCompras.BW.Interfaces.DA
+{
+    public class IGestionListaDA
+    {
+        Task<bool> CrearLista(ListaCompra lista);
+
+        //listar listas por varios metodos
+        Task<List<ListaCompra>> ObtenerListasActivas();
+        Task<List<ListaCompra>> ObtenerListasPorFechas(DateTime fecha);
+        Task<bool> EliminarLista(Guid idLista);
+        Task<bool> ExisteListaConNombre(string nombre, DateTime fecha);
+    }
+}
